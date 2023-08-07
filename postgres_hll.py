b6a12103194489f04ae09a886b9fcea00da2180b
jhchee <cheehong05090@gmail.com>
2023-08-07 23:41:35 +0800

    update with create table command

--- postgres_hll.py
+++ postgres_hll.py
@@ -12,16 +12,19 @@ db_params = {
 try:
     conn = psycopg2.connect(**db_params)
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM helloworld WHERE id = 1")
-    cursor.execute("INSERT INTO helloworld(id, set) VALUES (1, hll_empty());")
+    # prepare the table
+    cursor.execute("DROP TABLE IF EXISTS public.visitor")
+    cursor.execute("""CREATE TABLE visitor (event_date TEXT, set hll)""")
+    cursor.execute("DELETE FROM visitor WHERE event_date = '2023-07-01'")
+    cursor.execute("INSERT INTO visitor(event_date, set) VALUES ('2023-07-01', hll_empty())")
 
     start_time = time.perf_counter()
     for _ in range(1, 1000):
         id = bson.ObjectId().__str__()
         cursor.execute("""
-                      UPDATE helloworld 
+                      UPDATE visitor 
                       SET set = hll_add(set, hll_hash_text(%s))
-                      WHERE id=1
+                      WHERE event_date = '2023-07-01'
                       """, (id,))
         conn.commit()
     end_time = time.perf_counter()
@@ -29,8 +32,9 @@ try:
     print(f'Function Took {total_time:.4f} seconds')
 
     # print result
-    cursor.execute("SELECT hll_cardinality(set) FROM helloworld WHERE id = 1")
+    cursor.execute("SELECT hll_cardinality(set) FROM visitor WHERE event_date = '2023-07-01'")
     result = cursor.fetchone()
+    print(result)
     # Close the cursor and connection
     cursor.close()
     conn.close()
--- redis_hll.py
+++ redis_hll.py
@@ -8,9 +8,12 @@ try:
     start_time = time.perf_counter()
     for _ in range(1, 1000):
         id = bson.ObjectId().__str__()
-        rc.execute_command(f"pfadd hll2 {id}")
+        rc.execute_command(f"pfadd visitor_2023_07_01 {id}")
     end_time = time.perf_counter()
     total_time = end_time - start_time
     print(f'Function Took {total_time:.4f} seconds')
+    res = rc.execute_command("pfcount visitor_2023_07_01")
+    print(res)
+
 except redis.ConnectionError as e:
     print(f"Error: {e}")
